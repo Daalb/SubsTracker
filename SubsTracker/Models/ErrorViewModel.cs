6ac50393131e204e0f3b2467b57b607b3bdc8137
Daalb <diegoaalborrojas@gmail.com>
2025-10-22 07:39:18 -0500

    WIP-Configuring project

---
+++ SubsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
