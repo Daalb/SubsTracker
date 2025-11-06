52610eed7d34537b0ddee3aff1eb29f2dcdc54e8
Daalb <diegoaalborrojas@gmail.com>
2025-11-06 07:55:45 -0500

    - Added a migration to add seed data for subscription entity

--- SubsTracker/Database/SubsTrackerContext.cs
+++ SubsTracker/Database/SubsTrackerContext.cs
@@ -14,4 +14,23 @@ public class SubsTrackerContext(
 {
     // DbSets for each entity. Entities represent tables in the database.
     public DbSet<SubscriptionEntity> Subscriptions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SubscriptionEntity>(entity =>
+        {
+            entity.HasData(
+                new SubscriptionEntity { 
+                    Id  = "2513c007-b0c0-4740-88ca-be481c8c1065",
+                    Name = "Netflix",
+                    Description = "Streaming service for movies and TV shows",
+                    Value = 19900m,
+                    Currency = "COP",
+                    Category = "Entertainment",
+                    PaymentDate = new DateTime(2025, 11, 19),
+                    Frecuency = Models.Enums.FrecuencyEnum.Monthly
+                }
+            );
+        });
+    }
 }
---
+++ SubsTracker/Migrations/20251106125258_Added_OneSubscription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SubsTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_OneSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Subscriptions",
+                columns: new[] { "Id", "Category", "Currency", "Description", "Frecuency", "Name", "PaymentDate", "Value" },
+                values: new object[] { "2513c007-b0c0-4740-88ca-be481c8c1065", "Entertainment", "COP", "Streaming service for movies and TV shows", 2, "Netflix", new DateTime(2025, 11, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), 19900m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Subscriptions",
+                keyColumn: "Id",
+                keyValue: "2513c007-b0c0-4740-88ca-be481c8c1065");
+        }
+    }
+}
